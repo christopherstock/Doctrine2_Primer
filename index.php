a7c7ce569a1f5d394d7f0255ce73ad21918a07f1
christopherstock <email@christopherstock.de>
2017-03-18 19:25:37 +0100

    Updated TODO list.

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
  *  TODO ASAP Refactor: Remove redundancies in the action system!
  *  TODO ASAP Refactor: Cleaner separation of calculation and output?
  *  TODO ASAP Create class 'ActionMenuItem'
- *  TODO ASAP Use model ids in model names/descriptions?
+ *  TODO ASAP Use model ids for setting them into model names/descriptions?
  *  TODO ASAP Create new action 'get entity manager' with debug output (for 1st workshop lesson ..)?
  *  TODO ASAP Create wiki page with ERD and action descriptions.
  *  TODO ASAP Prepare knowledge for linking a new project with a git repository.
